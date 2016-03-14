61e045076d606f4e1b64ace0ede14566c292946a
yanli6 <liyan.code@gmail.com>
2016-03-14 17:29:43 +0800

    backup_offset config

---
+++ conf/backup_config.rb
@@ -0,0 +1,89 @@
+# config what to backup
+backup_list=[
+    {
+        :db_host=>'localhost',
+        :db_host_psql_username=>'username',
+        :db_list=>[
+            {
+                :db_name=>'rep_tes',
+                :table_list=>[
+                    {
+                        :table_name=>'table_01',
+                        :offset_column_name=>'col_01',
+                        :offset_column_type=>'type_01'
+                    },
+                    {
+                        :table_name=>'table_02',
+                        :offset_column_name=>'col_01',
+                        :offset_column_type=>'type_01'
+                    }
+                ]
+            },
+            {
+                :db_name=>'rep_tes',
+                :table_list=>[
+                    {
+                        :table_name=>'table_01',
+                        :offset_column_name=>'col_01',
+                        :offset_column_type=>'type_01'
+                    },
+                    {
+                        :table_name=>'table_02',
+                        :offset_column_name=>'col_01',
+                        :offset_column_type=>'type_01'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        :db_host=>'host_02',
+        :db_host_psql_username=>'username02',
+        :db_list=>[
+            {
+                :db_name=>'rep_test',
+                :table_list=>[
+                    {
+                        :table_name=>'table_01',
+                        :offset_column_name=>'col_01',
+                        :offset_column_type=>'type_01'
+                    },
+                    {
+                        :table_name=>'table_02',
+                        :offset_column_name=>'col_01',
+                        :offset_column_type=>'type_01'
+                    }
+                ]
+            },
+            {
+                :db_name=>'rep_tes',
+                :table_list=>[
+                    {
+                        :table_name=>'table_01',
+                        :offset_column_name=>'col_01',
+                        :offset_column_type=>'type_01'
+                    },
+                    {
+                        :table_name=>'table_02',
+                        :offset_column_name=>'col_01',
+                        :offset_column_type=>'type_01'
+                    }
+                ]
+            }
+        ]
+    }
+]
+# test p
+backup_list.each{|host|
+  p host[:db_host]
+  p host[:db_host_psql_username]
+  db_list=host[:db_list]
+  db_list.each{|db|
+    p "  "+db[:db_name]
+    table_list=db[:table_list]
+    table_list.each{|table|
+      p "    "+table[:table_name]
+      p "    "+table[:offset_column_name]
+    }
+  }
+}
